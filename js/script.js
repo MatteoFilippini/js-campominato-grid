df3504531c9cd49dcc7c1950d7df86e77d29dad6
MatteoFilippini <matteofilippini8@gmail.com>
2022-01-17 15:47:03 +0100

    taken user choice

--- js/script.js
+++ js/script.js
@@ -5,3 +5,14 @@ con difficoltà 1 => tra 1 e 100
 con difficoltà 2 => tra 1 e 81
 con difficoltà 3 => tra 1 e 49
 */
+
+// PRENDO LA SCELTA DELL UTENTE
+
+const userChoice = document.getElementById('choice');
+const buttonGen = document.getElementById('buttonGen');
+
+
+buttonGen.addEventListener('click', function () {
+    const userChoiceValue = userChoice.value;
+    console.log(userChoiceValue);
+})
