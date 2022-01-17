8a39eee5ae77463e5156228031cd6374b3c923fe
MatteoFilippini <matteofilippini8@gmail.com>
2022-01-17 15:40:51 +0100

    imposted HTML page

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*
+L'utente indica TRAMITE DOM un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, 
+in cui ogni cella contiene un numero tra quelli compresi in un range:
+con difficoltà 1 => tra 1 e 100
+con difficoltà 2 => tra 1 e 81
+con difficoltà 3 => tra 1 e 49
+*/
